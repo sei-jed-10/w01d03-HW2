bc52499e4d98cd2a59d4f95e7c4a8e3926a03ec4
Amal337 <amal.grergi@icloud.com>
2019-11-20 07:28:22 +0300

    created js hm file

---
+++ HW2.js
@@ -0,0 +1,38 @@
+///E: 1 ////
+// 1- name : .length
+//   description: it returns the number of indexes in a string
+//   example: 
+  var name = "amal"
+  var y = name.length()
+  console.log(y)
+// 2-  name : .slice
+//   description: it takes a part of a string and returns the portion in a new string
+//   example: 
+var str = "yes please, come in. I am just watching Friends and working on my homework. multitasking is cool"
+var n = str.slice(25, 30)
+
+console.log(n)
+// 3- name : .replace
+//   description: it replace a part of a value with another value
+//   example: 
+var a = " hey! let's go to the movies"
+ var b = a.replace("the movies", "space")
+
+   console.log(b)
+
+///// E: 2 //////
+// anywhere inside function y
+
+///// E: 3 /////      I could nt find the solution :((( I ll work more on it on weekend
+function rollDice(num1, num2) {
+    // var num1 =
+    // var num2 =
+    // var sum = num1 + num2
+       returns(rollDice.Math.floor(Math.random(num1 + num2)))
+   }
+  
+   rollDice(1, 6)
+
+////// E: 4 ////////   I am sorry i did nt solve this as time is up and I have to push this before 8 am 
+function currencyConverter(sr, le)
+  if 
